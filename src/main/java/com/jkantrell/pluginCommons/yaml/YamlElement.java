29719ebeb41756e77d4dcd47b2bbd1b5373f6a84
Jeshua Reyes <jeshuakrc@gmail.com>
2022-03-10 01:38:09 -0600

    Added 'YamlElement', 'YamlElementType' and 'YamlMap', which, along with 'List<YamlElement' represent data strictures in Yaml.

---
+++ src/main/java/com/jkantrell/pluginCommons/yaml/YamlElement.java
@@ -0,0 +1,101 @@
+package com.jkantrell.pluginCommons.yaml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Represents a Yaml data element, including Scalars, Maps and Lists.
+ */
+public class YamlElement {
+
+    //FIELDS
+    protected final YamlElementType type;
+    protected final Object element;
+
+    //CONSTRUCTORS
+
+    /**
+     * Creates a new YamlElement from a Java object. Only objects that can be assigned as a Yaml primitive, such as Java primitives
+     * and Strings will succeed, otherwise, an exception will be thrown.
+     *
+     * @param element The object to represent as Yaml type.
+     *
+     * @throws IllegalArgumentException if the passed object cannot be interpreted as a Yaml type.
+     */
+    public YamlElement(Object element) {
+        this.element = element;
+        YamlElementType<?> type = YamlElementType.assign(element);
+        AbstractYamlConfig.LOGGER.info(element.getClass().toString());
+        if (type == null) { throw new IllegalArgumentException("Not assignable to Yaml scalar"); }
+        this.type = type;
+    }
+
+    /**
+     * Checks if the element is of a given YamlType.
+     *
+     * @param type the type to check
+     * @return true if the type provided matches the element's type, false otherwise.
+     */
+    public boolean is(YamlElementType<?> type) {
+        AbstractYamlConfig.LOGGER.info(this.type.getType().toString() + "\n" + type.getType().toString());
+        return this.type.equals(type);
+    }
+
+    /**
+     * Gets the element casted as the Java class represented by the specified Yaml type, if the object is actually of that type.
+     *
+     * @param type The Yaml type to get the element as.
+     * @return The element. Null if the element is not of the specified type.
+     */
+    public <T> T get(YamlElementType<T> type) {
+        if (this.is(type)) {
+            return (T) this.element;
+        }
+        return null;
+    }
+
+    /**
+     * Gets the raw object contained in this YamlElement.
+     *
+     * @return the object.
+     */
+    public Object get() {
+        return element;
+    }
+
+    /**
+     * Gets a list of all the sub-Yaml elements contained in this Yaml element, all casted to the Java class represented by the
+     * specified Yaml type.
+     *
+     * Useful under the certainty of this element being a list and all the elements in it are of the same Yaml primitive type.
+     *
+     * @param type The type to get all elements in the list as.
+     * @return The list.
+     * @throws ClassCastException If this element is not a list or any of the elements inside is not of the specified type.
+     */
+    public <T> List<T> getListOf(YamlElementType<T> type) throws ClassCastException {
+        if (!this.is(YamlElementType.LIST)) { throw new ClassCastException("Not a list."); }
+        List<YamlElement> list = this.get(YamlElementType.LIST);
+        List<T> tList = new ArrayList<>();
+        list.forEach(elm -> {
+            T object = elm.get(type);
+            if (object != null) {
+                tList.add(object);
+            } else {
+                throw new ClassCastException(
+                        "Element of index " + list.indexOf(elm) + " is " + elm.type.getType().getSimpleName() +
+                        ", expected all to be of type " + type.getType().getSimpleName() + ". Unable to parse list."
+                );
+            }
+        });
+
+        return tList;
+    }
+
+    @Override
+    public String toString() {
+        return "[Type: " + this.type.getType().getSimpleName() + ", Value: " + element.toString() + "]";
+    }
+
+}
---
+++ src/main/java/com/jkantrell/pluginCommons/yaml/YamlElementType.java
@@ -0,0 +1,88 @@
+package com.jkantrell.pluginCommons.yaml;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A fake-enum class that represents all possible primitive data types in Yaml, including Scalars, Lists and Dictionaries.
+ */
+public class YamlElementType<E> {
+
+    //FAKE ENUM VALUES
+    public static final YamlElementType<Integer> INT = new YamlElementType<>(Integer.class);
+    public static final YamlElementType<Double> DOUBLE = new YamlElementType<>(Double.class);
+    public static final YamlElementType<String> STRING = new YamlElementType<>(String.class);
+    public static final YamlElementType<Boolean> BOOL = new YamlElementType<>(Boolean.class);
+    public static final YamlElementType<List<YamlElement>> LIST = new YamlElementType<>(List.class);
+    public static final YamlElementType<YamlMap> MAP = new YamlElementType<>(Map.class);
+
+    private static final YamlElementType<?>[] values_ = {INT, DOUBLE,STRING,BOOL,LIST,MAP};
+
+    /**
+     * Gets all the values of the fake-enum. Each one representing a type of primitive data in Yaml.
+     *
+     * @return the values as an array.
+     */
+    public static YamlElementType<?>[] values() {
+        return values_;
+    }
+
+    private static final Map<Class<?>, YamlElementType<?>> PRIMITIVE_MAP = Map.ofEntries(
+            Map.entry(int.class,INT),
+            Map.entry(float.class, DOUBLE),
+            Map.entry(long.class, INT),
+            Map.entry(double.class, DOUBLE),
+            Map.entry(short.class, INT),
+            Map.entry(char.class,STRING),
+            Map.entry(boolean.class,BOOL)
+    );
+    private final Class<?> type_;
+
+    private YamlElementType(Class<?> type) {
+        this.type_ = type;
+    }
+
+    /**
+     * Gets the java Class mapped to this Yaml type.
+     *
+     * @return the Class object.
+     */
+    public Class<?> getType(){
+        return this.type_;
+    }
+
+    /**
+     * Gets the YamlElementType object that can be assigned to the object provided.
+     *
+     * @param object the object to assign.
+     * @return the YamlElementType object. Null if non-assignable.
+     */
+    public static YamlElementType<?> assign(Object object) {
+        YamlElementType<?> r = null;
+        Class<?> clazz = object.getClass();
+
+        if (object.getClass().isArray()) {
+            return YamlElementType.LIST;
+        }
+        if (YamlElementType.PRIMITIVE_MAP.containsKey(clazz)) {
+            return YamlElementType.PRIMITIVE_MAP.get(clazz);
+        }
+        for (YamlElementType<?> type : YamlElementType.values()) {
+            if (type.getType().isAssignableFrom(clazz)) {
+                return type;
+            }
+        }
+        AbstractYamlConfig.LOGGER.info(object.getClass().getSimpleName() + " is not YAML type assignable.");
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) { return true; }
+        if (object == null) { return false; }
+        if (object.getClass().equals(this.getClass())) {
+            return ((YamlElementType) object).getType().equals(this.getType());
+        }
+        return false;
+    }
+}
